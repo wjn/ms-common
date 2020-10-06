c02c63d1eb8b486d22464567d254e167ff7c300a
Will Nielsen <48866+wjn@users.noreply.github.com>
2020-10-06 16:09:50 -0500

    handling msg that is object OR string

--- src/logger/classes/logger.ts
+++ src/logger/classes/logger.ts
@@ -1,13 +1,18 @@
 import { LogType } from '../enums/LogType';
 class LogIt {
   private type: LogType = LogType.INFO;
-  private msg: string = '';
+  private msg: string | object = '';
 
   // outputs to the console --> logger-it
-  out(type: LogType, msg: string) {
+  out(type: LogType, msg: string | object) {
     this.type = type;
     this.msg = msg;
-    console.log(`[${this.type}] ${this.msg}`);
+    let prefix = `[${this.type}]`;
+    if (typeof msg === 'object') {
+      console.log(`[${prefix}]`, this.msg);
+    } else {
+      console.log(`[${prefix}] ${this.msg}`);
+    }
   }
 }
 
