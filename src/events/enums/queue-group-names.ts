b68d5600d430a36ea1964cf55e9739d1653d14ae
Will Nielsen <48866+wjn@users.noreply.github.com>
2020-11-02 07:31:29 -0600

    added expiration queue group name

--- src/events/enums/queue-group-names.ts
+++ src/events/enums/queue-group-names.ts
@@ -2,4 +2,5 @@ export enum queueGroupNames {
   ORDERS_SERVICE = 'orders-service',
   TICKETS_SERVICE = 'tickets-service',
   PAYMENTS_SERVICE = 'payment-service',
+  EXPIRATION_SERVICE = 'expiration-service',
 }
