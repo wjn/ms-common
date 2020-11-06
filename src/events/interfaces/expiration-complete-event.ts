0a0298fb9a013efa838cbc4d7f1b82d5ed5461e1
Will Nielsen <48866+wjn@users.noreply.github.com>
2020-11-06 07:19:47 -0600

    updted data to data: OrderData interface

--- src/events/interfaces/expiration-complete-event.ts
+++ src/events/interfaces/expiration-complete-event.ts
@@ -1,8 +1,7 @@
 import { Topics } from '../enums/topics';
+import { OrderData } from './order-data';
 
 export interface ExpirationCompleteEvent {
   topic: Topics.ExpirationComplete;
-  data: {
-    orderId: string;
-  };
+  data: OrderData;
 }
